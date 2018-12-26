7de2f2afa23a8ca0f68a9fc6209d10268b01a1b2
FearDog <FearDog@bk.ru>
2018-12-26 16:02:11 +0500

    Создал все кнопки и поле ввода. Написал процедуру обработки кнопки +/-

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,281 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.btn_1 = new System.Windows.Forms.Button();
+      this.btn_2 = new System.Windows.Forms.Button();
+      this.btn_3 = new System.Windows.Forms.Button();
+      this.btn_4 = new System.Windows.Forms.Button();
+      this.btn_5 = new System.Windows.Forms.Button();
+      this.btn_6 = new System.Windows.Forms.Button();
+      this.btn_7 = new System.Windows.Forms.Button();
+      this.btn_8 = new System.Windows.Forms.Button();
+      this.btn_9 = new System.Windows.Forms.Button();
+      this.btn_C = new System.Windows.Forms.Button();
+      this.btn_Del = new System.Windows.Forms.Button();
+      this.btn_Rev = new System.Windows.Forms.Button();
+      this.btn_Plus = new System.Windows.Forms.Button();
+      this.btn_Minus = new System.Windows.Forms.Button();
+      this.btn_Mult = new System.Windows.Forms.Button();
+      this.btn_Mod = new System.Windows.Forms.Button();
+      this.btn_0 = new System.Windows.Forms.Button();
+      this.btn_Point = new System.Windows.Forms.Button();
+      this.btn_Result = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(14, 12);
+      this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(178, 20);
+      this.textBox1.TabIndex = 0;
+      // 
+      // btn_1
+      // 
+      this.btn_1.Location = new System.Drawing.Point(14, 76);
+      this.btn_1.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_1.Name = "btn_1";
+      this.btn_1.Size = new System.Drawing.Size(37, 24);
+      this.btn_1.TabIndex = 1;
+      this.btn_1.Text = "1";
+      this.btn_1.UseVisualStyleBackColor = true;
+      // 
+      // btn_2
+      // 
+      this.btn_2.Location = new System.Drawing.Point(61, 76);
+      this.btn_2.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_2.Name = "btn_2";
+      this.btn_2.Size = new System.Drawing.Size(37, 24);
+      this.btn_2.TabIndex = 2;
+      this.btn_2.Text = "2";
+      this.btn_2.UseVisualStyleBackColor = true;
+      // 
+      // btn_3
+      // 
+      this.btn_3.Location = new System.Drawing.Point(108, 76);
+      this.btn_3.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_3.Name = "btn_3";
+      this.btn_3.Size = new System.Drawing.Size(37, 24);
+      this.btn_3.TabIndex = 3;
+      this.btn_3.Text = "3";
+      this.btn_3.UseVisualStyleBackColor = true;
+      // 
+      // btn_4
+      // 
+      this.btn_4.Location = new System.Drawing.Point(14, 110);
+      this.btn_4.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_4.Name = "btn_4";
+      this.btn_4.Size = new System.Drawing.Size(37, 24);
+      this.btn_4.TabIndex = 4;
+      this.btn_4.Text = "4";
+      this.btn_4.UseVisualStyleBackColor = true;
+      // 
+      // btn_5
+      // 
+      this.btn_5.Location = new System.Drawing.Point(61, 110);
+      this.btn_5.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_5.Name = "btn_5";
+      this.btn_5.Size = new System.Drawing.Size(37, 24);
+      this.btn_5.TabIndex = 5;
+      this.btn_5.Text = "5";
+      this.btn_5.UseVisualStyleBackColor = true;
+      // 
+      // btn_6
+      // 
+      this.btn_6.Location = new System.Drawing.Point(108, 110);
+      this.btn_6.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_6.Name = "btn_6";
+      this.btn_6.Size = new System.Drawing.Size(37, 24);
+      this.btn_6.TabIndex = 6;
+      this.btn_6.Text = "6";
+      this.btn_6.UseVisualStyleBackColor = true;
+      // 
+      // btn_7
+      // 
+      this.btn_7.Location = new System.Drawing.Point(14, 144);
+      this.btn_7.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_7.Name = "btn_7";
+      this.btn_7.Size = new System.Drawing.Size(37, 24);
+      this.btn_7.TabIndex = 7;
+      this.btn_7.Text = "7";
+      this.btn_7.UseVisualStyleBackColor = true;
+      // 
+      // btn_8
+      // 
+      this.btn_8.Location = new System.Drawing.Point(61, 144);
+      this.btn_8.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_8.Name = "btn_8";
+      this.btn_8.Size = new System.Drawing.Size(37, 24);
+      this.btn_8.TabIndex = 8;
+      this.btn_8.Text = "8";
+      this.btn_8.UseVisualStyleBackColor = true;
+      // 
+      // btn_9
+      // 
+      this.btn_9.Location = new System.Drawing.Point(108, 144);
+      this.btn_9.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_9.Name = "btn_9";
+      this.btn_9.Size = new System.Drawing.Size(37, 24);
+      this.btn_9.TabIndex = 9;
+      this.btn_9.Text = "9";
+      this.btn_9.UseVisualStyleBackColor = true;
+      // 
+      // btn_C
+      // 
+      this.btn_C.Location = new System.Drawing.Point(14, 42);
+      this.btn_C.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_C.Name = "btn_C";
+      this.btn_C.Size = new System.Drawing.Size(37, 24);
+      this.btn_C.TabIndex = 10;
+      this.btn_C.Text = "C";
+      this.btn_C.UseVisualStyleBackColor = true;
+      // 
+      // btn_Del
+      // 
+      this.btn_Del.Location = new System.Drawing.Point(61, 42);
+      this.btn_Del.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_Del.Name = "btn_Del";
+      this.btn_Del.Size = new System.Drawing.Size(37, 24);
+      this.btn_Del.TabIndex = 11;
+      this.btn_Del.Text = "<--";
+      this.btn_Del.UseVisualStyleBackColor = true;
+      // 
+      // btn_Rev
+      // 
+      this.btn_Rev.Location = new System.Drawing.Point(108, 42);
+      this.btn_Rev.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_Rev.Name = "btn_Rev";
+      this.btn_Rev.Size = new System.Drawing.Size(37, 24);
+      this.btn_Rev.TabIndex = 12;
+      this.btn_Rev.Text = "+/-";
+      this.btn_Rev.UseVisualStyleBackColor = true;
+      this.btn_Rev.Click += new System.EventHandler(this.btn_Rev_Click);
+      // 
+      // btn_Plus
+      // 
+      this.btn_Plus.Location = new System.Drawing.Point(155, 42);
+      this.btn_Plus.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_Plus.Name = "btn_Plus";
+      this.btn_Plus.Size = new System.Drawing.Size(37, 24);
+      this.btn_Plus.TabIndex = 13;
+      this.btn_Plus.Text = "+";
+      this.btn_Plus.UseVisualStyleBackColor = true;
+      // 
+      // btn_Minus
+      // 
+      this.btn_Minus.Location = new System.Drawing.Point(155, 76);
+      this.btn_Minus.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_Minus.Name = "btn_Minus";
+      this.btn_Minus.Size = new System.Drawing.Size(37, 24);
+      this.btn_Minus.TabIndex = 14;
+      this.btn_Minus.Text = "-";
+      this.btn_Minus.UseVisualStyleBackColor = true;
+      // 
+      // btn_Mult
+      // 
+      this.btn_Mult.Location = new System.Drawing.Point(155, 110);
+      this.btn_Mult.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_Mult.Name = "btn_Mult";
+      this.btn_Mult.Size = new System.Drawing.Size(37, 23);
+      this.btn_Mult.TabIndex = 15;
+      this.btn_Mult.Text = "*";
+      this.btn_Mult.UseVisualStyleBackColor = true;
+      // 
+      // btn_Mod
+      // 
+      this.btn_Mod.Location = new System.Drawing.Point(155, 144);
+      this.btn_Mod.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_Mod.Name = "btn_Mod";
+      this.btn_Mod.Size = new System.Drawing.Size(37, 23);
+      this.btn_Mod.TabIndex = 16;
+      this.btn_Mod.Text = "/";
+      this.btn_Mod.UseVisualStyleBackColor = true;
+      // 
+      // btn_0
+      // 
+      this.btn_0.Location = new System.Drawing.Point(14, 178);
+      this.btn_0.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_0.Name = "btn_0";
+      this.btn_0.Size = new System.Drawing.Size(37, 24);
+      this.btn_0.TabIndex = 17;
+      this.btn_0.Text = "0";
+      this.btn_0.UseVisualStyleBackColor = true;
+      // 
+      // btn_Point
+      // 
+      this.btn_Point.Location = new System.Drawing.Point(61, 178);
+      this.btn_Point.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_Point.Name = "btn_Point";
+      this.btn_Point.Size = new System.Drawing.Size(37, 24);
+      this.btn_Point.TabIndex = 18;
+      this.btn_Point.Text = ".";
+      this.btn_Point.UseVisualStyleBackColor = true;
+      // 
+      // btn_Result
+      // 
+      this.btn_Result.Location = new System.Drawing.Point(109, 178);
+      this.btn_Result.Margin = new System.Windows.Forms.Padding(5);
+      this.btn_Result.Name = "btn_Result";
+      this.btn_Result.Size = new System.Drawing.Size(84, 24);
+      this.btn_Result.TabIndex = 19;
+      this.btn_Result.Text = "=";
+      this.btn_Result.UseVisualStyleBackColor = true;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.ClientSize = new System.Drawing.Size(207, 214);
+      this.Controls.Add(this.btn_Result);
+      this.Controls.Add(this.btn_Point);
+      this.Controls.Add(this.btn_0);
+      this.Controls.Add(this.btn_Mod);
+      this.Controls.Add(this.btn_Mult);
+      this.Controls.Add(this.btn_Minus);
+      this.Controls.Add(this.btn_Plus);
+      this.Controls.Add(this.btn_Rev);
+      this.Controls.Add(this.btn_Del);
+      this.Controls.Add(this.btn_C);
+      this.Controls.Add(this.btn_9);
+      this.Controls.Add(this.btn_8);
+      this.Controls.Add(this.btn_7);
+      this.Controls.Add(this.btn_6);
+      this.Controls.Add(this.btn_5);
+      this.Controls.Add(this.btn_4);
+      this.Controls.Add(this.btn_3);
+      this.Controls.Add(this.btn_2);
+      this.Controls.Add(this.btn_1);
+      this.Controls.Add(this.textBox1);
+      this.Name = "Form1";
       this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
     }
 
     #endregion
+
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button btn_1;
+    private System.Windows.Forms.Button btn_2;
+    private System.Windows.Forms.Button btn_3;
+    private System.Windows.Forms.Button btn_4;
+    private System.Windows.Forms.Button btn_5;
+    private System.Windows.Forms.Button btn_6;
+    private System.Windows.Forms.Button btn_7;
+    private System.Windows.Forms.Button btn_8;
+    private System.Windows.Forms.Button btn_9;
+    private System.Windows.Forms.Button btn_C;
+    private System.Windows.Forms.Button btn_Del;
+    private System.Windows.Forms.Button btn_Rev;
+    private System.Windows.Forms.Button btn_Plus;
+    private System.Windows.Forms.Button btn_Minus;
+    private System.Windows.Forms.Button btn_Mult;
+    private System.Windows.Forms.Button btn_Mod;
+    private System.Windows.Forms.Button btn_0;
+    private System.Windows.Forms.Button btn_Point;
+    private System.Windows.Forms.Button btn_Result;
   }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,6 +12,19 @@ namespace WindowsFormsApp1
 {
   public partial class Form1 : Form
   {
+    float a, b;
+    int oper;
+    bool znak = true;
+
+    private void btn_Rev_Click(object sender, EventArgs e)
+    {
+      if (znak)
+        textBox1.Text = "-" + textBox1.Text;
+      else
+        textBox1.Text = textBox1.Text.Replace("-", "");
+      znak = !znak;
+    }
+
     public Form1()
     {
       InitializeComponent();
